430caa9f010b0c1f9f76f18237398a86d50dc3f2
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2021-05-06 00:17:51 +0500

    After add boilder setup for less and css files

--- src/addElement.js
+++ src/addElement.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 
-import "./addElement.css"
-const createFooElement = ()=>{
-    const foo = document.createElement('p');
+import "./addElement.css";
+import "./addE.less";
+const createFooElement = (tag)=>{
+    const foo = document.createElement(tag);
     foo.innerHTML = _.join(['Heeloo','CarbonTeq']);
     return foo;
 }
-document.body.appendChild(createFooElement());
+document.body.appendChild(createFooElement('p'));
+document.body.appendChild(createFooElement('div'));
